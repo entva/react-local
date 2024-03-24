c29c80e022732637c0fd3bbc12e16375f64dc481
Max Degterev <max@degterev.me>
2024-03-24 20:06:51 +0100

    export raw translate

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 export { useLocaleData, useActiveLocale, useT, useTranslate } from './hooks';
 export * from './types';
 
+export { default as translate } from './translate';
 export { default } from './components/international';
 export { default as T } from './components/t';
 export { default as Dictionary } from './components/dictionary';
